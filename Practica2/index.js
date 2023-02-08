ec7e8433b57570dba5565ee5b843c85856b624b8
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-02-08 09:37:16 -0600

    Hola

---
+++ Practica2/index.js
@@ -0,0 +1,9 @@
+let resultado = document.getElementById('resultado');
+
+const asignar = (valor) => resultado.value += valor;
+
+const  calcular = () =>
+{
+    if(resultado.value!='')
+        resultado.value = eval(resultado.value);
+}
