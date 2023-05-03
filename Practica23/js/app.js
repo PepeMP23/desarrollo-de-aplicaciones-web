4406df515cafde0dc4c1508ca913538ac037757a
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-05-03 09:49:32 -0600

    Practica 23

---
+++ Practica23/js/app.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            contador : 0
+        }
+    },
+    methods : {
+        incrementar(){
+            this.contador++;
+        }
+    }
+}).mount('#miApp');
