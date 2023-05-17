aa4dff85faa74e06ac19c339e809d9ea802f241d
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-05-17 11:45:09 -0600

    Vue

--- Practica34/js/app.js
+++ Practica34/js/app.js
@@ -12,7 +12,8 @@ const articulos = {
                 <tr v-for="articulo in datos">
                   <td>{{articulo.codigo}}</td>
                   <td>{{articulo.descripcion}}</td>
-                  <td>{{articulo.precio}}</td>
+                  <td v-if="articulo.categoria == 'verduras'">{{articulo.precio}}</td>
+                  <td v-else>Precio por kilo</td>
                 </tr>
               </table>
             `
@@ -25,40 +26,44 @@ createApp({
   },
   data() {
     return {
-        verduras: [
+        articulos: [
             {
                 codigo: 1,
                 descripcion: 'papas',
-                precio: 12.52
+                precio: 12.52,
+                categoria: 'verduras'
             }, 
             {
                 codigo: 2,
                 descripcion: 'naranjas',
-                precio: 21
+                precio: 21,
+                categoria: 'verduras'
             }, 
             {
                 codigo: 3,
                 descripcion: 'peras',
-                precio: 18.20
+                precio: 18.20,
+                categoria: 'verduras'
+            },
+            {
+                codigo: 4,
+                descripcion: 'arrachera',
+                precio: 12.52,
+                categoria: 'carnes'
+            }, 
+            {
+                codigo: 5,
+                descripcion: 'sirloin',
+                precio: 21,
+                categoria: 'carnes'
+            }, 
+            {
+                codigo: 6,
+                descripcion: 'angus',
+                precio: 18.20,
+                categoria: 'carnes'
             }
-        ],
-        carnes: [
-          {
-              codigo: 1,
-              descripcion: 'arrachera',
-              precio: 12.52
-          }, 
-          {
-              codigo: 2,
-              descripcion: 'sirloin',
-              precio: 21
-          }, 
-          {
-              codigo: 3,
-              descripcion: 'angus',
-              precio: 18.20
-          }
-      ]
+      ],  
     }
   }
 }).mount('#miApp');
