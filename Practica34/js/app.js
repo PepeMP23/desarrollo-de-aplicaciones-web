b29cbbbe23a45e05aa738211406d6284a0a9f628
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-05-16 22:08:46 -0600

    Vue

--- Practica34/js/app.js
+++ Practica34/js/app.js
@@ -25,7 +25,7 @@ createApp({
   },
   data() {
     return {
-        datos: [
+        verduras: [
             {
                 codigo: 1,
                 descripcion: 'papas',
@@ -41,7 +41,24 @@ createApp({
                 descripcion: 'peras',
                 precio: 18.20
             }
-        ]
+        ],
+        carnes: [
+          {
+              codigo: 1,
+              descripcion: 'arrachera',
+              precio: 12.52
+          }, 
+          {
+              codigo: 2,
+              descripcion: 'sirloin',
+              precio: 21
+          }, 
+          {
+              codigo: 3,
+              descripcion: 'angus',
+              precio: 18.20
+          }
+      ]
     }
   }
 }).mount('#miApp');
---
+++ proyecto1/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Hola Vue</h1>
+    <p>Bienvenido</p>
+  </div>
+</template>
+
+
