803f15e6ac2f91614aab532cfb107a0072ac0077
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-05-23 09:36:21 -0600

    Pracitca42

---
+++ Practica42/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Contador</h2>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+    <p>{{ contador }} elevado a la segunda potencia es: {{ calcularMejor }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      contador: 2,
+    }
+  },
+  methods:{
+    calcular(){
+      console.log('Entro a calcular');
+      return this.contador * this.contador
+    }
+  },
+  computed: {
+    calcularMejor() {
+      console.log('Entro a calcular mejor');
+      return this.contador * this.contador;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
